f2b03cf0fceb9fb731379feebe097d5804535562
Pedro Ramos <pedroramosg09@gmail.com>
2023-06-01 23:06:25 +0200

    ✨ feat(topbar): basic structure

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,35 @@
+import SvgSun from '../../icons/Sun';
+import './Topbar.scss';
+
+function Topbar() {
+	return (
+		<>
+			<header className="topbar">
+				<span className="logo">Pedro Ramos</span>
+				<nav className="nav">
+					<ul className="nav--items nav__actions">
+						<li className="nav--item">
+							<a href="#locale" className="nav--item__locale">
+								en
+							</a>
+						</li>
+						<li className="nav--item">
+							<SvgSun />
+						</li>
+						<li className="nav--item">menu</li>
+					</ul>
+					<ul className="nav--items nav__menu">
+						<li className="nav--item">home</li>
+						<li className="nav--item">about</li>
+						<li className="nav--item">work</li>
+						<li className="nav--item">contact</li>
+						<li className="nav--item">resume</li>
+						<li className="nav--item">close</li>
+					</ul>
+				</nav>
+			</header>
+		</>
+	);
+}
+
+export default Topbar;
