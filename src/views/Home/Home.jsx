b634cf3218ee41fd2c6f3f2fa268f0e1763c67ae
Pedro Ramos <pedroramosg09@gmail.com>
2023-06-03 20:17:34 +0200

    💄 feat(home): home info added and styled

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,7 +4,11 @@ function Home() {
 	return (
 		<>
 			<main className="home">
+				<big className="home__prefix">Hello, I&apos;m</big>
 				<h1 className="home__name">Pedro Ramos</h1>
+				<h2 className="home__title h4">
+					Front-End Web Developer <br /> Multimedia Engineer
+				</h2>
 			</main>
 		</>
 	);
