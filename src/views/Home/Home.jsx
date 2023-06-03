6df8a1b4fe3688d6526a78d8e2f1c95836d79f64
Pedro Ramos <pedroramosg09@gmail.com>
2023-06-03 20:34:38 +0200

    💄 feat(home): socialmedia added

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,3 +1,9 @@
+import SvgGithub from '../../icons/Github';
+import SvgLinkedin from '../../icons/Linkedin';
+import SvgCodepen from '../../icons/Codepen';
+import SvgTwitter from '../../icons/Twitter';
+import SvgMail from '../../icons/Mail';
+
 import './Home.scss';
 
 function Home() {
@@ -9,6 +15,24 @@ function Home() {
 				<h2 className="home__title h4">
 					Front-End Web Developer <br /> Multimedia Engineer
 				</h2>
+
+				<div className="home__social">
+					<a href="https://github.com/pedrorxmos" className="cursor-target" target="_blank" rel="noreferrer">
+						<SvgGithub size="x-normal" />
+					</a>
+					<a href="https://www.linkedin.com/in/pedroramosguardiola/" className="cursor-target" target="_blank" rel="noreferrer">
+						<SvgLinkedin size="x-normal" />
+					</a>
+					<a href="https://codepen.io/pedrorxmos" className="cursor-target" target="_blank" rel="noreferrer">
+						<SvgCodepen size="x-normal" />
+					</a>
+					<a href="https://twitter.com/" className="cursor-target" target="_blank" rel="noreferrer">
+						<SvgTwitter size="x-normal" />
+					</a>
+					<a href="mailTo:pedrorxmosdev@gmail.com" className="cursor-target" target="_blank" rel="noreferrer">
+						<SvgMail size="x-normal" />
+					</a>
+				</div>
 			</main>
 		</>
 	);
