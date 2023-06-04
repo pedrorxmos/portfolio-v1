78d38b7cbd335b15991266df3b5e776a42fc84a5
Pedro Ramos <pedroramosg09@gmail.com>
2023-06-04 17:03:49 +0200

    💄 feat(work): basic structure, and image style

---
+++ src/views/Work/Work.jsx
@@ -0,0 +1,45 @@
+import { collection } from 'firebase/firestore';
+import { useFirestore, useFirestoreCollectionData } from 'reactfire';
+
+import SvgArrowDown from '../../icons/ArrowDown';
+import Footer from '../../components/Footer/Footer';
+import './Work.scss';
+
+function Work() {
+	const workData = collection(useFirestore(), 'projects');
+	const { data } = useFirestoreCollectionData(workData);
+	// console.log(data);
+
+	const url = '/public/img/about-me.webp';
+
+	return (
+		<>
+			<main className="work">
+				<div className="work__container">
+					<div className="work__img">
+						<div style={{ backgroundImage: `url('/public/img/${'about-me'}.webp')` }} className="selected-work"></div>
+					</div>
+					<div className="work__projects">
+						<div className="work__header">
+							<div className="work__title">
+								<h1>Featured Work</h1>
+								<small>click on the project to see more details</small>
+							</div>
+							<div className="work__filter">
+								<span>Filter:</span>
+								<button className="cursor-target dropdown">
+									All
+									<SvgArrowDown size="x-small" title="dropdown icon" />
+								</button>
+							</div>
+						</div>
+						<div className="work__list">the projects list</div>
+					</div>
+				</div>
+			</main>
+			<Footer />
+		</>
+	);
+}
+
+export default Work;
