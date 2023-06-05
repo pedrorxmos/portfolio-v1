da33f8c77f6cc10d01a8c5aaefb179b477bba9ac
Pedro Ramos <pedroramosg09@gmail.com>
2023-06-05 15:59:47 +0200

    💄 feat(project): detail style

--- src/views/Project/Project.jsx
+++ src/views/Project/Project.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import { doc } from 'firebase/firestore';
 import { useFirestore, useFirestoreDocData } from 'reactfire';
@@ -7,6 +7,9 @@ import Footer from '../../components/Footer/Footer';
 import Button from '../../components/Button/Button';
 import './Project.scss';
 import SvgHash from '../../icons/Hash';
+import SvgHeart from '../../icons/Heart';
+import SvgArrowLeft from '../../icons/ArrowLeft';
+import SvgArrowRight from '../../icons/ArrowRight';
 
 function Project() {
 	const id = useParams().id;
@@ -18,6 +21,23 @@ function Project() {
 	return (
 		<>
 			<main className="project-detail main">
+				<div className="detail__actions">
+					<Link to="/work" className="action">
+						<SvgArrowLeft />
+						back
+					</Link>
+
+					<div className="actions__other">
+						<Link className="action">
+							<SvgArrowLeft />
+							previous
+						</Link>
+						<Link className="action">
+							next
+							<SvgArrowRight />
+						</Link>
+					</div>
+				</div>
 				<div className="detail__section">
 					<div className="detail__img">
 						<img src={`/img/${data?.img}.webp`} alt={`${data?.title} image`} />
@@ -25,10 +45,10 @@ function Project() {
 
 					<div className="detail__info">
 						<div className="info__title">
+							<h1>{data?.title}</h1>
 							<div className="info__tags">
-								<h1>{data?.title}</h1>
 								{data?.tags.map((tag, index) => (
-									<div key={index} className="project__tag" style={{ '--tag-color': tag.color }}>
+									<div key={index} className="info__tag" style={{ '--tag-color': tag.color }}>
 										<SvgHash title="hash icon" size="x-small" />
 										{tag.tag}
 									</div>
@@ -44,23 +64,32 @@ function Project() {
 				</div>
 
 				<div className="detail__section">
-					<div className="detail__details">
-						<h2 className="h3">Details</h2>
-						<span>
-							<small>year:</small> {data?.year}
-						</span>
-						<span>
-							<small>design:</small> {data?.design.value}
-						</span>
+					<div className="detail__card">
+						<SvgHeart size="x-big" title="tools" />
+						<div className="card__info">
+							<h2 className="h3">Details</h2>
+							<span>
+								<small>category:</small> e-commerce
+							</span>
+							<span>
+								<small>year:</small> {data?.year}
+							</span>
+							<span>
+								<small>design:</small> {data?.design.value}
+							</span>
+						</div>
 					</div>
-					<div className="detail__details">
-						<h2 className="h3">Tools</h2>
-						<div className="detail__tools">
-							{data?.tools.map((tool, index) => (
-								<span key={index} className="detail__tool">
-									{tool}
-								</span>
-							))}
+					<div className="detail__card">
+						<SvgHeart size="x-big" title="tools" />
+						<div className="card__info">
+							<h2 className="h3">Tools used</h2>
+							<div className="detail__tools">
+								{data?.tools.map((tool, index) => (
+									<span key={index} className="detail__tool">
+										{tool}
+									</span>
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
