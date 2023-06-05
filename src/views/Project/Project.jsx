4361cd41005b632bc6c249a353145a866705693d
Pedro Ramos <pedroramosg09@gmail.com>
2023-06-05 13:04:28 +0200

    ✨ feat(project): connect project to firestore

--- src/views/Project/Project.jsx
+++ src/views/Project/Project.jsx
@@ -1,8 +1,16 @@
 import { useParams } from 'react-router-dom';
+
+import { doc } from 'firebase/firestore';
+import { useFirestore, useFirestoreDocData } from 'reactfire';
+
 import Footer from '../../components/Footer/Footer';
 
 function Project() {
 	const id = useParams().id;
+	const aboutData = doc(useFirestore(), 'projects', id);
+	const { data } = useFirestoreDocData(aboutData);
+
+	console.log(data);
 
 	return (
 		<>
