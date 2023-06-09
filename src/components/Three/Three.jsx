fcfae34ce31ff38fe6ea7126c605a292d9caebcd
Pedro Ramos <pedroramosg09@gmail.com>
2023-06-09 14:26:43 +0200

    ✨ feat(three): add 3dmodels

--- src/components/Three/Three.jsx
+++ src/components/Three/Three.jsx
@@ -1,7 +1,13 @@
 import { Canvas } from '@react-three/fiber';
 import './Three.scss';
 import Eye from '../../models/Eye';
+import BlobOne from '../../models/BlobOne';
+import BlobTwo from '../../models/BlobTwo';
+import BlobThree from '../../models/BlobThree';
+import LineOne from '../../models/LineOne';
+import LineTwo from '../../models/LineTwo';
 import { use100vh } from 'react-div-100vh';
+import Light from './Light';
 
 function Three() {
 	return (
@@ -10,8 +16,13 @@ function Three() {
 				<Canvas orthographic camera={{ position: [0, 0, 10], left: -2, right: 2, top: 2, bottom: -2, zoom: 100, far: 2000, near: 1 }}>
 					<ambientLight intensity={0.6} />
 					<hemisphereLight args={['#fff', '#333', 0.5]} />
-					<directionalLight color="hsl(60, 10%, 96%)" intensity={0.6} position={[0, 0.1, 1]} />
+					<Light />
 					<Eye />
+					<BlobOne />
+					<BlobTwo />
+					<BlobThree />
+					<LineOne />
+					<LineTwo />
 				</Canvas>
 			</div>
 		</>
